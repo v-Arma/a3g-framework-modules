799bb0c06cafd81af967e7ba6d135d358ec58c51
Cephei <Cephel@users.noreply.github.com>
2015-03-11 12:49:05 +0100

    Create module.hpp

---
+++ a3g-safezone/module.hpp
@@ -0,0 +1,10 @@
+#ifdef A3G_Framework_state_Module
+  class Safezone {
+    name = "Safezone";
+    authors[] = {"Cephei"};
+  };
+#endif
+
+#ifdef A3G_Framework_state_Functions
+  #include "cfgFunctions.hpp"
+#endif
