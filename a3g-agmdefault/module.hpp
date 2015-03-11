75c8ace1b0cdbaf6c82f916eaf5656740f4515e8
Cephei <Cephel@users.noreply.github.com>
2015-03-11 12:52:25 +0100

    Create module.hpp

---
+++ a3g-agmdefault/module.hpp
@@ -0,0 +1,10 @@
+#ifdef A3G_Framework_state_Module
+class AGMSettings {
+  name = "New default AGM medical settings";
+  authors[] = {"Cephei"};
+};
+#endif
+
+#ifdef A3G_Framework_state_Functions
+  #include "cfgFunctions.hpp"
+#endif
