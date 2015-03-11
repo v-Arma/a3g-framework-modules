264727cff2ff05ab2684e6fa7dcee397556bc3f1
Cephei <Cephel@users.noreply.github.com>
2015-03-11 15:28:51 +0100

    Create module.hpp

---
+++ a3g-antireslot/module.hpp
@@ -0,0 +1,10 @@
+#ifdef A3G_Framework_state_Module
+  class A3G_AntiReslot {
+    name = "Reslot prevention and JIP assistance script";
+    authors[] = {"Cephei"};
+  };
+#endif
+
+#ifdef A3G_Framework_state_Functions
+  #include "cfgFunctions.hpp"
+#endif
