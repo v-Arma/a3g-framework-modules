7130e425a56c368b40b032a3fd3fbf1625605195
Cephei <Cephel@users.noreply.github.com>
2015-03-11 12:48:19 +0100

    Create cfgFunctions.hpp

---
+++ a3g-safezone/cfgFunctions.hpp
@@ -0,0 +1,10 @@
+class A3G_Safezone {
+  class Functions {
+    file = "modules\a3g-safezone\functions";
+    class AssignEvent {
+      postInit = 1;
+    };
+    class MakeSafezone {};
+    class SafezoneEH {};
+  };
+};
