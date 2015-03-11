18c725988a7290fbcc88f3a396354edb1041897d
Cephei <Cephel@users.noreply.github.com>
2015-03-11 12:25:40 +0100

    Create cfgFunctions.hpp

---
+++ a3g-antiloot/cfgFunctions.hpp
@@ -0,0 +1,11 @@
+class A3G_AntiLoot {
+  class Functions {
+    file = "a3g-anti-loot\functions";
+    class LootLoop {};
+    class RemoveLoot {};
+    class SpawnLoops {
+      postInit = 1;
+    };    
+    class VehicleLoop {};
+  };
+};
