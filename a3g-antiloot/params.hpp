a8121092e5f7ef86180dbe247e8b56b966dd6626
Cephei <Cephel@users.noreply.github.com>
2015-03-11 12:41:55 +0100

    Update params.hpp

--- a3g-antiloot/params.hpp
+++ a3g-antiloot/params.hpp
@@ -1,8 +1,10 @@
-class A3G_Template_RemoveLoot {
-  title = "Prevent AI equipment from being picked up?";
-  values[] = { 0, 1, 2 };
-  texts[] = { "No", "Only weapons", "Everything" };
-  default = 1;
-  function = "A3G_AntiLoot_fnc_SetMode";
-  isGlobal = 0;
-};
+#ifdef A3G_AntiLoot_settings_UseParams
+  class A3G_Template_RemoveLoot {
+    title = "Prevent AI equipment from being picked up?";
+    values[] = { 0, 1, 2 };
+    texts[] = { "No", "Only weapons", "Everything" };
+    default = 1;
+    function = "A3G_AntiLoot_fnc_SetMode";
+    isGlobal = 0;
+  };
+#endif
