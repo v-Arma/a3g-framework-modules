3a1c55dad0e9a9a4af9dfb352dd26407031cd984
Cephei <cepheidev+github@gmail.com>
2015-03-12 19:05:33 +0100

    added modules.hpp

---
+++ modules.hpp
@@ -0,0 +1,13 @@
+/*
+	Author: Cephei
+	Description:
+	Add an include for each module you want to use
+	Example:
+	#include "modulename\module.hpp"
+	Notes:
+	module.hpp needs to be present and properly configured
+*/
+
+
+#include "standard-settings\module.hpp"
+#include "a3g-agmdefault\module.hpp"
