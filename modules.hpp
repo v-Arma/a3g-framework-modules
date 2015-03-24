5e923c60483a1ceca4012db64a6c04831566ac73
Cephei <Cephel@users.noreply.github.com>
2015-03-24 12:26:26 +0100

    Update modules.hpp

--- modules.hpp
+++ modules.hpp
@@ -1,11 +1,8 @@
 /*
   Author: Cephei
-  Description:
-  Add an include for each module you want to use
-  Example:
-  #include "modulename\module.hpp"
-  Notes:
-  module.hpp needs to be present and properly configured
+  Description: Add an include for each module you want to use
+  Example: #include "modulename\module.hpp"
+  Notes: module.hpp needs to be present and properly configured
 */
 
 
