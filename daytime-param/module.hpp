2daf246b38c3428888a0992ab36b88ff053e04c4
Cephei <Cephel@users.noreply.github.com>
2015-03-24 12:35:36 +0100

    Update module.hpp

--- daytime-param/module.hpp
+++ daytime-param/module.hpp
@@ -1,4 +1,4 @@
-#ifdef A3G_Framework_state_Module
+#ifdef A3GFramework_state_Module
   class DaytimeParam {
     name = "Day Time";
     description = "Adds a parameter to change the time of day during role selection.";
@@ -6,6 +6,6 @@
   };
 #endif
 
-#ifdef A3G_Framework_state_Params
+#ifdef A3GFramework_state_Params
   #include "params.hpp"
 #endif
