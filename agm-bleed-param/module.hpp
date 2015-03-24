14093220d03d5e74b26677611c08282e314a9077
Cephei <Cephel@users.noreply.github.com>
2015-03-24 12:33:58 +0100

    Update module.hpp

--- agm-bleed-param/module.hpp
+++ agm-bleed-param/module.hpp
@@ -1,4 +1,4 @@
-#ifdef A3G_Framework_state_Module
+#ifdef A3GFramework_state_Module
   class AGMBleedParam {
     name = "AGM Bleedout Parameter";
     description = "Adds a parameter to change the AGM bleedout time during role select.";
@@ -6,10 +6,10 @@
   };
 #endif
 
-#ifdef A3G_Framework_state_Functions
+#ifdef A3GFramework_state_Functions
   #include "cfgFunctions.hpp"
 #endif
 
-#ifdef A3G_Framework_state_Params
+#ifdef A3GFramework_state_Params
   #include "params.hpp"
 #endif
