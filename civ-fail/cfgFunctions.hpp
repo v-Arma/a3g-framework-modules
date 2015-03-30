5cefce9edb06439abc3447091f331e48e76ef2d9
Cephei <Cephel@users.noreply.github.com>
2015-03-30 11:01:06 +0200

    Update cfgFunctions.hpp

--- civ-fail/cfgFunctions.hpp
+++ civ-fail/cfgFunctions.hpp
@@ -1,6 +1,10 @@
 class CivFail {
   class Functions {
     file = "modules\civ-fail\functions";
+    class AssignEvent {};
     class HandleDeath {};
+    class SpawnLoop {
+      postInit = 1;
+    };
   };
 };
