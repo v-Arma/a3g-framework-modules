ec01939440f27a0f5b8ade30460559769b89abf8
Cephei <Cephel@users.noreply.github.com>
2015-03-30 11:11:47 +0200

    Create cfgDebriefing.hpp

---
+++ civ-fail/cfgDebriefing.hpp
@@ -0,0 +1,8 @@
+class End1 {
+	title = "Mission Failed";
+	subtitle = "You killed too many civilians";
+	description = "";
+	pictureBackground = "";
+	picture = "b_inf";
+	pictureColor[] = {0.0,0.3,0.6,1};
+};
