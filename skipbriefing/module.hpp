d21f3860fad9c3b2f1404d77f2ab4ea20e5e9a45
Cephei <Cephel@users.noreply.github.com>
2015-04-09 13:06:03 +0200

    Create module.hpp

---
+++ skipbriefing/module.hpp
@@ -0,0 +1,11 @@
+#ifdef A3GFramework_state_Module
+  class SkipBriefing {
+    name = "Skip Briefing";
+    description = "Skips the briefing on mission start.";
+    authors[] = {"Cephei"};
+  };
+#endif
+
+#ifdef A3GFramework_state_Functions
+  #include "cfgFunctions.hpp"
+#endif
